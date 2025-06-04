cf9a3c457fadc436bd42f7ec423602f50ec7f293
Rizwan Hanif <rizwanhanif633@gmail.com>
2025-06-04 22:23:14 +0500

    Implemented header, footer and hero section

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    
+  },
+  integrations: [react()]
+});
